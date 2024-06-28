c638c95532d3dc637e08dd35fdc5104c6dcd08b0
dongE <68381747+Daae-Kim@users.noreply.github.com>
2024-06-28 18:24:50 +0900

    FEAT: 나누기 기능입니다 (#39)
    * docs: src 폴더에 calc.java 추가
    
    * feat: 나누기 구현

---
+++ src/calc.java
@@ -0,0 +1,36 @@
+public class calc {
+    public static void div(int a, int b){
+        int result = a/b;
+        System.out.println("a / b = " + result);
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫번째 숫자를 입력해주세요");
+        int a = sc.nextInt();
+
+        System.out.println("두번째 숫자를 입력해주세요");
+        int b = sc.nextInt();
+
+        System.out.println("수행할 연산 기호를 입력해주세요(+, -, *, /, 종료)");
+        String act = sc.next();
+
+        switch(act){
+            case "종료":
+                break;
+            case "+":
+                add(a,b);
+                break;
+            case "-":
+                sub(a,b);
+                break;
+            case "*":
+                mul(a,b);
+                break;
+            case "/":
+                div(a,b);
+                break;
+        }
+    }
+}
