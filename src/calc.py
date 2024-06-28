a9734aaef2de69963d995324c49b76246a18ff74
이시현 <83636532+shihyun-Lee@users.noreply.github.com>
2024-06-28 18:34:18 +0900

    feat: mod 기능 추가 (#57)

--- src/calc.py
+++ src/calc.py
@@ -1,5 +1,11 @@
 
+def mod(a, b):
+    return a%b
+num1, num2= map(int,input('두개의 숫자를 입력해주세요 : ').split())
+print(mod(num1,num2))
+
 def add(a, b):
     return a + b
 num1, num2= map(int,input('두개의 숫자를 입력해주세요 : ').split())
 print(add(num1,num2))
+
